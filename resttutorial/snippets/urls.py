3a4941f546fa577f4b520faaaaadc6c3985f754e
Ankita Samanta <ankitasa@atrihub.io>
2024-11-20 14:24:26 -0800

    feat(Django REST Framework): Using Routers

--- resttutorial/snippets/urls.py
+++ resttutorial/snippets/urls.py
@@ -1,22 +1,13 @@
-from django.urls import path
-from .views import SnippetList, SnippetDetail, UserList, UserDetail, api_root, SnippetHighlight
-from rest_framework.urlpatterns import format_suffix_patterns
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import SnippetViewSet, UserViewSet
 
-urlpatterns = format_suffix_patterns([
-    path('', api_root),
-    path('snippets/',
-        SnippetList.as_view(),
-        name='snippet-list'),
-    path('snippets/<int:pk>/',
-        SnippetDetail.as_view(),
-        name='snippet-detail'),
-    path('snippets/<int:pk>/highlight/',
-        SnippetHighlight.as_view(),
-        name='snippet-highlight'),
-    path('users/',
-        UserList.as_view(),
-        name='user-list'),
-    path('users/<int:pk>/',
-        UserDetail.as_view(),
-        name='user-detail')
-])
+# Create a router and register our ViewSets with it.
+router = DefaultRouter()
+router.register(r'snippets', SnippetViewSet, basename='snippet')
+router.register(r'users', UserViewSet, basename='user')
+
+# The API URLs are now determined automatically by the router.
+urlpatterns = [
+    path('', include(router.urls)),
+]
