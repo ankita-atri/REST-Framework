af024fdf31818c31f35779f297d70f5f51e1f65a
Ankita Samanta <ankitasa@atrihub.io>
2024-11-20 14:11:42 -0800

    feat(Django REST Framework): Relationships & Hyperlinked APIs

--- resttutorial/snippets/urls.py
+++ resttutorial/snippets/urls.py
@@ -1,12 +1,22 @@
 from django.urls import path
-from .views import SnippetList, SnippetDetail, UserList, UserDetail
+from .views import SnippetList, SnippetDetail, UserList, UserDetail, api_root, SnippetHighlight
 from rest_framework.urlpatterns import format_suffix_patterns
 
-urlpatterns = [
-    path('snippets/', SnippetList.as_view()),
-    path('snippets/<int:pk>/', SnippetDetail.as_view()),
-path('users/', UserList.as_view()),
-path('users/<int:pk>/', UserDetail.as_view()),
-]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
+urlpatterns = format_suffix_patterns([
+    path('', api_root),
+    path('snippets/',
+        SnippetList.as_view(),
+        name='snippet-list'),
+    path('snippets/<int:pk>/',
+        SnippetDetail.as_view(),
+        name='snippet-detail'),
+    path('snippets/<int:pk>/highlight/',
+        SnippetHighlight.as_view(),
+        name='snippet-highlight'),
+    path('users/',
+        UserList.as_view(),
+        name='user-list'),
+    path('users/<int:pk>/',
+        UserDetail.as_view(),
+        name='user-detail')
+])
